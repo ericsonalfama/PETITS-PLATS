a4b6a2838819689157cbfa0c707bc91366fb8522
Ericson <ericsonalfama@gmail.com>
2024-06-17 19:01:13 +0200

    utils.js update

--- scripts/utils.js
+++ scripts/utils.js
@@ -4,21 +4,17 @@
 //----------------------------------------------------
 // NORMALISER LA CHAÎNE DE CARACTÈRES
 //----------------------------------------------------
-/* function normalizeText(text) {
-  return text.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').trim();
-} */
 
 function normalizeText(text) {
   return text
-    .toLowerCase()  // Converte todo o texto para minúsculas
-    .normalize('NFD')  // Normaliza o texto
-    .replace(/[\u0300-\u036f]/g, '')  // Remove acentos e diacríticos
-    .trim()  // Remove espaços em branco no início e no final
-    .split(' ')  // Divide o texto em palavras
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))  // Capitaliza a primeira letra de cada palavra
-    .join(' ');  // Junta as palavras de volta em uma string
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .trim()
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
 }
-
 
 // Fonction pour récupérer les appareils
 function getDescription() {
